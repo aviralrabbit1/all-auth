e87c5a53f5bc1d19af02b386bd7fc24f18c7af09
aviralrabbit1 <aviral.robin@gmail.com>
2025-10-24 20:39:09 +0530

    🌏Implement configuration management and environment variable handling

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,19 +1,31 @@
-import dotenv from 'dotenv';
-dotenv.config();
+// import dotenv from 'dotenv';
+// dotenv.config();
 
-import express from 'express';
+import express, { Request, Response} from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import { config } from './config/app.config';
 
 const app = express();
+// const BASE_PATH = config.BASE_PATH;
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-  origin: process.env.APP_ORIGIN, //sending cookies/credentials to client side
+  origin: config.APP_ORIGIN , //sending cookies/credentials to client side
   credentials: true,
 }));
 
 app.use(cookieParser());
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({
+    message: "Backend is running...",
+  });
+});
+
+app.listen(config.PORT, async () => {
+  console.log("Server listening on PORT: ", config.PORT);
+})
